26fc155101dffaf94a48c6eb447e7ba22e56ae24
Derrick <plainenough@users.noreply.github.com>
2018-12-10 10:22:24 -0700

    Update build.py

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import time
 print("Hello World, this is the setup of a pipeline")
-time.sleep(10)
-print("We slept for 20 seconds")
+time.sleep(1)
+print("We slept for 2 seconds")
